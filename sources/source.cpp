bc2327661b5a291707e322d479e7ffd13498ddd7
hucker99 <54907176+hucker99@users.noreply.github.com>
2019-12-16 23:13:52 +0300

    Update source.cpp

--- sources/source.cpp
+++ sources/source.cpp
@@ -84,8 +84,6 @@ Json::parse_array(const std::string &str, size_t &pos) {
                 state = Act::find_value;
             }
         } else if (isdigit(str[i]) || isalpha(str[i])) {
-            if ((str[i] == '+' && isdigit(str[i + 1]) != 0)
-                || (str[i] == '-' && isdigit(str[i + 1])) != 0) i++;
             if (str[i] >= '0' && str[i] <= '9') {
                 if (state == Act::find_value) {
                     res.emplace_back(parse_number(str, i));
@@ -152,8 +150,6 @@ Json::parse_object(const std::string &str, size_t &pos) {
                 state = Act::find_value;
             }
         } else if (isdigit(str[i]) || isalpha(str[i])) {
-            if ((str[i] == '+'|| str[i] == '-')
-                  && isdigit(str[i + 1])) i++;
             if (str[i] >= '0' && str[i] <= '9') {
                 if (state == Act::find_value) {
                     res[key] = parse_number(str, i);
